b323632f441b1a28867e143f314dd31ea77eb46e
Cheryl Yvonne <42008873+cy-Yvonne@users.noreply.github.com>
2019-03-09 12:49:02 -0500

    Add files via upload

---
+++ java/HelloProject.java
@@ -0,0 +1,18 @@
+
+package helloproject;
+
+
+public class HelloProject {
+
+
+    public static void main(String[] args) {
+       // System.out.println("Hello");
+       /* System.out.print("Hello");
+        System.out.println("com 125");
+        System.out.println((15+5)+" is the result of 15 + 5");*/
+        int num1 = 5;
+        int num2 = 6;
+        System.out.println((num1)+"*"+(num2)+"="+(num1*num2));
+    }
+    
+}
