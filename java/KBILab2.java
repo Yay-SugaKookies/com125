3981acaf75ded9b353b26d5ac0f408d637184771
Cheryl Yvonne <42008873+cy-Yvonne@users.noreply.github.com>
2019-03-30 12:51:35 -0400

    Add files via upload

---
+++ java/KBILab2.java
@@ -0,0 +1,22 @@
+
+package kbilab2;
+
+import java.util.Scanner;
+
+
+public class KBILab2 {
+
+    public static void main(String[] args) {
+       Scanner keyboard = new Scanner(System.in);
+       System.out.println("Give me a word!");
+       keyboard.next();
+       System.out.println("Give me a second word!");
+       keyboard.next();
+       System.out.println("Great, now ypur favorite number?");
+       keyboard.next();
+       System.out.println("And your second-favorite number...");
+       keyboard.next();
+       System.out.println("Whew! wasn't that fun?");
+    }
+    
+}
